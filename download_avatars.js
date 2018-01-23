307fca1e8fcc5790fe1e3c18e7b5206e5a80d088
Lijing Wang <lijing@ualberta.ca>
2018-01-23 16:20:37 +0000

    secreat token authorization with user-agent info

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,21 @@
 var request = require('request');
+var token = require('./secret');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 
 var getRepoContributors = function(repoOwner, repoName, cb){
-  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  request.get(url);
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GrinJessie',
+      'Authorization':token.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body){
+    cb(err, body);
+  });
 };
 
 
