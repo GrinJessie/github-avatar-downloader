2e9cc7177cdab1a03c7d0cb42ae68b1a00b4c5e9
Lijing Wang <lijing@ualberta.ca>
2018-01-23 07:26:11 +0000

    add get request and query params

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,17 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-// var getRepoContributors = function(callback){
 
-// };
+
+
+var getRepoContributors = function(repoOwner, repoName, cb){
+  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request.get(url);
+};
+
+
+getRepoContributors ("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+})
+
